8e86e33f6dba97dd779a26d993ba8e0023c12821
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-05-21 00:10:46 +0700

    not found page

--- front_end/src/pages/notFound/NotFound.jsx
+++ front_end/src/pages/notFound/NotFound.jsx
@@ -1,11 +1,25 @@
+import React from 'react';
+import { Button, Row, Col } from 'antd'
+import { Link } from "react-router-dom";
+import imageNotFound from "../../assets/img/404.svg";
+import './styles.scss'
 
-
-const NotFound = () =>{
-    return(
-        <div>
-            Not Found Page
-        </div>
-    )
+export default function NotFound() {
+  return (
+    <Row className="not-found-container">
+      <Col xl={12} xs={24} className="img">
+          <img alt="Not Found" src={imageNotFound}/>
+      </Col>
+      <Col xl={12} xs={24} className="content">
+        <h1>UH OH! Có gì đó sai sai.</h1>
+        <br/>
+        <p>Trang bạn đang tìm kiếm không tồn tại. Làm thế nào bạn có được ở đây là một bí ẩn. Nhưng bạn có thể nhấp vào nút bên dưới để quay lại trang chủ.
+        </p>
+        <br/>
+        <Link to ="/">
+            <Button type="ghost" >HOME</Button>
+        </Link>
+      </Col>
+    </Row>
+  )
 }
-
-export default NotFound
