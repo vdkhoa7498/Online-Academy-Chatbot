42b0325a7a1b43ac4f01dbe4cfd123d678b5cb7c
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-05-30 01:37:05 +0700

    update logo header bar

--- front_end/src/components/headerBar/HeaderBar.jsx
+++ front_end/src/components/headerBar/HeaderBar.jsx
@@ -1,5 +1,6 @@
 import { Input, Button } from 'antd'
 import { Link } from 'react-router-dom'
+import Logo from '../../assets/img/logo_64.png';
 import './styles.scss'
 
 const { Search } = Input
@@ -7,7 +8,7 @@ const { Search } = Input
 const HeaderBar = () =>{
     return(
         <div className="header-bar">
-            <Link to="/" className="logo-item">Online Learning</Link>
+            <Link to="/" className="logo-item"><img src={Logo} alt="Online Learning" /> Online Learning</Link>
             <div className="tool">
                 <Search placeholder="Enter your input..." className="search-box"/>
                 <Button shape="round" type="primary">Login</Button>
