7cc51976705fb6a1f56296a2705140233a9be431
vdkhoa7498 <vdkhoa7498@gmail.com>
2021-06-06 00:40:05 +0700

    get profile when load or reload any page

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,12 +1,29 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import RouterOutlet from './routers';
 import { ConfigProvider } from 'antd';
+import { getProfile } from './stores/auth';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import 'antd/dist/antd.css'
 import './App.scss';
 
-function App() {
+function App(props) {
+
+  useEffect(()=>{
+    if (localStorage && localStorage.getItem('access_token')) {
+      props.getProfile({
+        onSuccess: () => {
+          localStorage.setItem("isAuthenticated", true)
+        },
+        onFailure: () => {
+          localStorage.setItem("isAuthenticated", false)
+        }
+      });
+    } else {
+    //  props.toggleGlobalLoading(false);
+    }
+  }, [])
+
   return (
     <div className="App">
       <ConfigProvider>
@@ -21,6 +38,6 @@ const mapState = (state) => ({
 });
 
 const mapDispatch = dispatch => bindActionCreators({
-  
+  getProfile
 }, dispatch)
 export default connect(mapState, mapDispatch)(App);
