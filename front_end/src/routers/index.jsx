2021a3f92941093f0e36756b081b3bf9d78ffd73
Dang Khoa <khoavo@sbl.dev>
2021-08-09 22:33:41 +0700

    create new course

--- front_end/src/routers/index.jsx
+++ front_end/src/routers/index.jsx
@@ -33,7 +33,7 @@ import CourseDetail from "../pages/courseDetail/CourseDetail";
 import CourseListCategory from "../pages/courseListCategory/CourseListCategory";
 import WatchList from "../pages/watchList/Watchlist";
 import Profile from "../pages/profile/Profile";
-import PostCourse from "../pages/postCourse/PostCourse";
+import PostCourse from "../pages/lecturer/postCourse/PostCourse";
 import Learning from "../pages/Leaning/Learning";
 import MyCourses from "../pages/myCourses/MyCourses";
 
@@ -47,6 +47,7 @@ import AdminRoute from "./AdminRoute";
 import StudentRoute from "./StudentRoute";
 import LecturerRoute from "./LecturerRoute";
 import PrivateRoute from "./privateRoute";
+import ListCourse from "../pages/lecturer/listCourse/ListCourse";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -167,7 +168,7 @@ function LecturerRouteLayout(props) {
             defaultSelectedKeys={["1"]}
           >
             <Menu.Item key="1">
-              <Link to="/lecturer/create-new-course">
+              <Link to="/lecturer/my-courses">
                 <AppstoreOutlined />
                 <span>Khoá học của tôi</span>
               </Link>
@@ -282,11 +283,17 @@ function RouterOutlet(props) {
             </RouteLayout>
           </StudentRoute>
 
-          <LecturerRoute exact path={["/lecturer/create-new-course"]}>
+          <LecturerRoute
+            exact
+            path={["/lecturer/create-new-course", "/lecturer/my-courses"]}
+          >
             <LecturerRouteLayout>
               <Switch>
                 <Route exact path="/lecturer/create-new-course">
                   <PostCourse />
+                </Route>
+                <Route exact path="/lecturer/my-courses">
+                  <ListCourse />
                 </Route>
               </Switch>
             </LecturerRouteLayout>
